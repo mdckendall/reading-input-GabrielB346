e36bcecf566bd1594c72b2f6fac58af6341dbbed
Gabriel Bosi <113543034+GabrielB346@users.noreply.github.com>
2022-09-21 22:50:11 +0000

    Made the work

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
     System.out.println("Enter a word:");
+    String word=scan.nextLine();
     
+    int count = word.length();
+
+    System.out.println(count);
     //Your code will return the length of the entered String
   }
 }
